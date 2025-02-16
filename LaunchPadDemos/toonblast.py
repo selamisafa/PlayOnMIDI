76e5296214c199bfac4080009b36517860f6a554
selamisafa <selamisafa1999@hotmail.com>
2025-02-16 17:46:47 +0300

    Update toonblast.py

--- LaunchPadDemos/toonblast.py
+++ LaunchPadDemos/toonblast.py
@@ -8,131 +8,155 @@ lp = launchpad_py.Launchpad()
 lp.Open(0)
 lp.Reset()
 
-board = {'h': 8, 'l': 8}
+board = {'length': 8, 'height': 8}
 blockColors = [[3,0], [0,3], [1,1], [3,3]]
 currentBoard = []
 allBlocks = []
 currentBlockColor = [0,0]
 gravity = 1
 
-pickedX = -1
-pickedY = -1
-
 isGameOver = False
 
 def GenerateAllBoard():
-    global allblocks
-
-    for h in range(board['h']):
-        allBlocks.append([])
-        for l in range(board['l']):
-            allBlocks[h].append(deepcopy(blockColors[random.randrange(len(blockColors))]))
- 
-def MoveDown(pickedBlockCoordinates):
     global allBlocks
     
-    x = pickedBlockCoordinates[0]
+    for l in range(board['length']):
+        allBlocks.append([])
+
+        for h in range(board['height']):
+            allBlocks[l].append(deepcopy(blockColors[random.randrange(len(blockColors))]))
+ 
+def MoveDown(pickedBlockX):
+    global allBlocks
+    
     foundEmpty = False
     foundSomethingToMove = False
 
-    i = board['h'] - 1
-    while i >= 0:
+    y = board['height'] - 1
+    while y >= 0:
         if foundEmpty:
-            allBlocks[i+1][x] = deepcopy(allBlocks[i][x])
+            allBlocks[pickedBlockX][y+1] = deepcopy(allBlocks[pickedBlockX][y])
             foundSomethingToMove = True
-            if i == 0:
-                allBlocks[i][x] = [0,0]
-        elif allBlocks[i][x] == [0,0]:
+            if y == 0:
+                allBlocks[pickedBlockX][y] = [0,0]
+        elif allBlocks[pickedBlockX][y] == [0,0]:
             foundEmpty = True
 
-        i -= 1
+        y -= 1
     
-    allBlocks[0][x] = deepcopy(blockColors[random.randrange(len(blockColors))])
+    allBlocks[pickedBlockX][0] = deepcopy(blockColors[random.randrange(len(blockColors))])
+    Render()
 
     if foundEmpty and foundSomethingToMove:
-        MoveDown(pickedBlockCoordinates)
+        MoveDown(pickedBlockX)
 
     
 def TryBlast(pickedBlockCoordinates):
     global allBlocks
-
-    allBlocks[pickedBlockCoordinates[1]][pickedBlockCoordinates[0]] = [0,0]
-    MoveDown(pickedBlockCoordinates)
-    print(pickedBlockCoordinates)
-    print(allBlocks[pickedBlockCoordinates[1]][pickedBlockCoordinates[0]])
-    print(currentBoard[pickedBlockCoordinates[1]][pickedBlockCoordinates[0]])
+    theChunk = []
+    theChunk = CollectConnectedSame(pickedBlockCoordinates, theChunk)
     
-def CollectConnectedSame(pickedBlockCoordinates):
+    uniqueColumns = []
+
+    if len(theChunk) > 3:
+        for block in theChunk:
+            allBlocks[block[0]][block[1]] = [0,0]
+
+            if block[0] not in uniqueColumns:
+                uniqueColumns.append(block[0])
+
+    for column in uniqueColumns:
+        MoveDown(column)
+    
+def CollectConnectedSame(pickedBlockCoordinates, theChunk):
+    global allBlocks
+
     x = pickedBlockCoordinates[0]
     y = pickedBlockCoordinates[1]
-    
-    pickedBlock = allBlocks[y][x]
-    
-    #Up
-    
 
-def CanMoveDown(prediction):
-    for block in prediction:
-        if block[1] >= board["h"]:
-            return False
-    return True
-    
+    pickedBlock = allBlocks[x][y]
+    theChunk.append(pickedBlockCoordinates)
+
+    #Up
+    if y - 1 >= 0:
+        if pickedBlock == allBlocks[x][y - 1] and [x, y - 1] not in theChunk:
+            theChunk += CollectConnectedSame([x, y - 1], theChunk)
+
+    #Down
+    if y + 1 < board['height']:
+        if pickedBlock == allBlocks[x][y + 1] and [x, y + 1] not in theChunk:
+            theChunk += CollectConnectedSame([x, y + 1], theChunk)
+
+    #Left
+    if x - 1 >= 0:
+        if pickedBlock == allBlocks[x - 1][y] and [x - 1, y] not in theChunk:
+            theChunk += CollectConnectedSame([x - 1, y], theChunk)
+
+    #Right
+    if x + 1 < board['length']:
+        if pickedBlock == allBlocks[x + 1][y] and [x + 1, y] not in theChunk:
+            theChunk += CollectConnectedSame([x + 1, y], theChunk)
+
+    return theChunk
+
 def Render():
     global currentBoard
     global isGameOver
     global allBlocks
     
-    currentBoard = [[]]
-    for l in range(board['l']):
-        currentBoard[0].append(' ')
+    currentBoard = []
+    emptyList = []
+    
+    for h in range(board['height']):
+        emptyList.append(' ')
 
-    for h in range(board['h'] - 1):
-        currentBoard.append(deepcopy(currentBoard[0]))
+    for l in range(board['length']):
+        currentBoard.append(deepcopy(emptyList))        
 
     #adding the fucking blocks
-    y = 0
-    for row in allBlocks:
-        x = 0
-        for column in row:
-            if allBlocks[y][x] == [3,0]:
-                currentBoard[y][x] = "#"
-            elif allBlocks[y][x] == [0,3]:
-                currentBoard[y][x] = "O"
-            elif allBlocks[y][x] == [1,1]:
-                currentBoard[y][x] = "X"
-            elif allBlocks[y][x] == [3,3]:
-                currentBoard[y][x] = "$"
+    x = 0
+    for column in allBlocks:
+        y = 0
+        for block in column:
+            if allBlocks[x][y] == [3,0]:
+                currentBoard[x][y] = "#"
+            elif allBlocks[x][y] == [0,3]:
+                currentBoard[x][y] = "O"
+            elif allBlocks[x][y] == [1,1]:
+                currentBoard[x][y] = "X"
+            elif allBlocks[x][y] == [3,3]:
+                currentBoard[x][y] = "$"
             else:
-                currentBoard[y][x] = " "
+                currentBoard[x][y] = " "
                 
-            x+=1
-        y+=1
+            y+=1
+        x+=1
             
-    #print("\033[H\033[J", end="")
-    print(*currentBoard, sep = "\n")
+    """print("\033[H\033[J", end="")
+    print(*currentBoard, sep = "\n")"""
     
     RenderOnLaunchpad()
 
 def RenderOnLaunchpad():
     global allBlocks
-    for x in range(board['l']):
-        for y in range(board['h']):
-            currentBlock = allBlocks[y][x]
-            lp.LedCtrlXY(y, x+1, currentBlock[0], currentBlock[1])                
+    for x in range(board['length']):
+        for y in range(board['height']):
+            currentBlock = allBlocks[x][y]
+            lp.LedCtrlXY(x, y+1, currentBlock[0], currentBlock[1])                
 
 def ButtonChecker():
     global lastButton
-    button = lp.ButtonStateXY()
+    button = deepcopy(lp.ButtonStateXY())
+    lp.ButtonFlush()
+    
     if len(button) > 0:
         if button[2] == False:
-            TryBlast([button[1] - 1, button[0]])
-        
-    lp.ButtonFlush()
+            TryBlast([button[0], button[1] - 1])
+            print(button)
 
 def Update():
     global isGameOver
-    global pickedX
-    global pickedY
+
     if isGameOver:
         return
 
